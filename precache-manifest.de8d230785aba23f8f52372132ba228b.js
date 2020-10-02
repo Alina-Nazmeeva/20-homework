dae76c6f12eebb85d65aa5eb860104d18f00d874
Alina Nazmeeva <ell122zmey@gmail.com>
2020-10-02 03:13:40 +0300

    Updates

---
+++ precache-manifest.de8d230785aba23f8f52372132ba228b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6b29eeaf11cb3fdfa592f100270684",
+    "url": "/20-homework/index.html"
+  },
+  {
+    "revision": "aa593692b78a6f184954",
+    "url": "/20-homework/static/css/main.fbc7e09e.chunk.css"
+  },
+  {
+    "revision": "6312f204caf863a8292b",
+    "url": "/20-homework/static/js/2.f47c2cd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/20-homework/static/js/2.f47c2cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa593692b78a6f184954",
+    "url": "/20-homework/static/js/main.d80db3e3.chunk.js"
+  },
+  {
+    "revision": "a4f7a9523c49083ab254",
+    "url": "/20-homework/static/js/runtime-main.45a2ec63.js"
+  },
+  {
+    "revision": "0eb9f4cb6dbc74edd8d469803fb1dec2",
+    "url": "/20-homework/static/media/Bikini.0eb9f4cb.jpg"
+  },
+  {
+    "revision": "f4c90763e022546b80020a3abf9218fe",
+    "url": "/20-homework/static/media/BikiniPost.f4c90763.jpg"
+  },
+  {
+    "revision": "faee03050dd75800aa25648cd9ccd024",
+    "url": "/20-homework/static/media/Cutie.faee0305.JPG"
+  },
+  {
+    "revision": "f17234756108a1f731e6378b119017aa",
+    "url": "/20-homework/static/media/Milka.f1723475.jpg"
+  },
+  {
+    "revision": "5c2d8dbef69f104b578abb66ec6867c0",
+    "url": "/20-homework/static/media/MilkaPost1.5c2d8dbe.jpg"
+  },
+  {
+    "revision": "84b8e2466705dd6171d21f81659b1713",
+    "url": "/20-homework/static/media/MilkaPost2.84b8e246.jpg"
+  },
+  {
+    "revision": "b9dc091733a726090844c0f0c44fb096",
+    "url": "/20-homework/static/media/Murka.b9dc0917.jpg"
+  },
+  {
+    "revision": "938f32369fe1c23d5ff380bba8a57b70",
+    "url": "/20-homework/static/media/MurkaPost1.938f3236.jpg"
+  },
+  {
+    "revision": "bf221a27da4089a8a075d95c616a570b",
+    "url": "/20-homework/static/media/MurkaPost2.bf221a27.jpg"
+  },
+  {
+    "revision": "c5cf7cd01af500a1d8093c483db3009e",
+    "url": "/20-homework/static/media/comments.c5cf7cd0.png"
+  },
+  {
+    "revision": "f375abe3a5908dd70cc9b61c51a9c352",
+    "url": "/20-homework/static/media/likes.f375abe3.png"
+  },
+  {
+    "revision": "252c5cbd028e1959ed792572a96da60e",
+    "url": "/20-homework/static/media/postBackground.252c5cbd.jpg"
+  },
+  {
+    "revision": "80bfdd9881b3ac7c94c82157bb5dc48e",
+    "url": "/20-homework/static/media/reposts.80bfdd98.png"
+  },
+  {
+    "revision": "5dec6e9e699f2b8f3b35a870d36062ef",
+    "url": "/20-homework/static/media/share.5dec6e9e.png"
+  }
+]);
